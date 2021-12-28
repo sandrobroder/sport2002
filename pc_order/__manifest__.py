0bb572d76374366de93e2b599ce385266c67e32b
Marta Vila <marta.vila@processcontrol.es>
2021-12-28 16:34:16 +0100

    ADD pc_order module

---
+++ pc_order/__manifest__.py
@@ -0,0 +1,20 @@
+{
+  "name"                 :  "Pc Order",
+  "summary"              :  """""",
+  "category"             :  "Website",
+  "version"              :  "2.3.11",
+  "sequence"             :  1,
+  "author"               :  "ProcessControl",
+  "license"              :  "Other proprietary",
+  "website"              :  "",
+  "description"          :  """""",
+  "depends"              :  [
+                             'sale',
+                            ],
+  "data"                 :  [
+                             'views/core/sale_order.xml',
+                            ],
+  "application"          :  True,
+  "installable"          :  True,
+  "auto_install"         :  False
+}
