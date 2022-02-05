b52fc2247e159d5785389e71598778d9f2c74564
PChectorcerezo <80783603+PChectorcerezo@users.noreply.github.com>
2022-02-05 17:37:26 +0100

    Add barcode to product

---
+++ EMIPRO_demo/sport2002_base/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Sport2002 Base",
+    "version": "14.0.1.0.0",
+    "sequence": 6,
+    "summary": "",
+    "description": "",
+    "depends": ["web",
+                'website_sale_attribute_filter_price',
+                'website_sale',
+                'theme_clarico_vega',
+                ],
+    "data": [
+        "views/shop.xml",
+        "templates/assets.xml",
+        "templates/product.xml",
+    ],
+    "demo": [
+    ],
+    "qweb": [
+    ],
+    "installable": True,
+}
