a4c362843dcd2199db13e1b129f96bfcce8f77e3
carolinafernandez-processcontrol <84458198+carolinafernandez-processcontrol@users.noreply.github.com>
2022-02-10 11:25:52 -0300

    comisiones por zona

---
+++ processcontrol_comissions_by_zone/models/sale_comission_mixin.py
@@ -0,0 +1,13 @@
+# Copyright 2014-2020 Tecnativa - Pedro M. Baeza
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import api, fields, models
+
+class SaleCommissionMixin(models.AbstractModel):
+    _inherit = "sale.commission.mixin"
+
+    def _prepare_agents_vals_partner(self, partner):
+        if self.partner.state_id:
+            if self.partner.state_id.agent_ids:
+                return [(0, 0, self._prepare_agent_vals(agent)) for agent in self.partner.state_id.agent_ids]
+        return [(0, 0, self._prepare_agent_vals(agent)) for agent in partner.agent_ids]
