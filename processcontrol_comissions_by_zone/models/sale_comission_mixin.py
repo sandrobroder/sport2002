2e4ce039cb673f0dd4ef58ebd9b78360e77ecce9
carolinafernandez-processcontrol <84458198+carolinafernandez-processcontrol@users.noreply.github.com>
2022-02-16 08:10:20 -0300

    Deshabilitar "Crear y editar Productos"

--- processcontrol_comissions_by_zone/models/sale_comission_mixin.py
+++ processcontrol_comissions_by_zone/models/sale_comission_mixin.py
@@ -11,3 +11,19 @@ class SaleCommissionMixin(models.AbstractModel):
             if partner.state_id.agent_ids:
                 return [(0, 0, self._prepare_agent_vals(agent)) for agent in partner.state_id.agent_ids]
         return [(0, 0, self._prepare_agent_vals(agent)) for agent in partner.agent_ids]
+
+
+# class SaleOrderLine(models.Model):
+#     _inherit = "sale.order.line"
+#
+#     @api.depends("order_id.partner_id","product_id")
+#     def _compute_agent_ids(self):
+#         self.agent_ids = False  # for resetting previous agents
+#         for record in self.filtered(lambda x: x.order_id.partner_id):
+#             if not record.commission_free:
+#                 if record.product_id.categ_id.agent_ids:
+#
+#                 else:
+#                     record.agent_ids = record._prepare_agents_vals_partner(
+#                         record.order_id.partner_id
+#                     )
