6ce96644cadb0aadaa69496ac689a66ce057a154
gquispepc <gerardo.quispe@processcontrol.es>
2022-04-21 14:50:56 -0500

    [FIX] import variant

--- processcontrol_sale_order/models/sale_order.py
+++
@@ -1,8 +0,0 @@
-from odoo import api, fields, models, _
-from odoo.exceptions import ValidationError
-
-
-# class import_product_var_wizard(models.TransientModel):
-#    _inherit = "import.product.var.wizard"
-#
-#    def
