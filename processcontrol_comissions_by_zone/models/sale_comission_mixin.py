c8ea6de72348b8cc8d4da9f8d657165e250f4c1f
carolinafernandez-processcontrol <carofermendez@gmail.com>
2022-04-25 10:08:52 -0300

    Update sale_comission_mixin.py

--- processcontrol_comissions_by_zone/models/sale_comission_mixin.py
+++ processcontrol_comissions_by_zone/models/sale_comission_mixin.py
@@ -10,23 +10,22 @@ class SaleOrderLine(models.Model):
     def _compute_agent_ids(self):
         agent_ids = []
         domain_agent_ids = []
-        self.agent_ids = False  # for resetting previous agents
+        # for resetting previous agents
         for record in self.filtered(lambda x: x.order_id.partner_id):
+            record.agent_ids = False
             partner = record.order_id.partner_id
             if not record.commission_free:
                 domain_agent_ids = record._prepare_agents_vals_partner(
                         record.order_id.partner_id
                     )
-                #chequeo si el producto seleccionado, tiene en la categoria configurado el agente y si tiene configurado agente en la provicia del cliente
-                if record.product_template_id.categ_id.agent_ids and partner.state_id and partner.state_id.agent_ids:
-                    for agent in record.product_template_id.categ_id.agent_ids:
-                        if domain_agent_ids:
-                            if (agent.id in partner.state_id.agent_ids.ids) and (agent.id in domain_agent_ids.ids) and agent not in agent_ids:
+                #chequeo que los agentes en la ficha del cliente, esten dentro de la marca:
+                if record.product_template_id.categ_id.agent_ids:
+                    if domain_agent_ids:
+                        for agent in record.product_template_id.categ_id.agent_ids:
+                            if (agent.id in domain_agent_ids):
                                 agent_ids.append(agent)
-                        else:
-                            if (agent.id in partner.state_id.agent_ids.ids) and agent not in agent_ids:
-                                agent_ids.append(agent)
-                    if agent_ids:
-                        record.agent_ids = [(0, 0, record._prepare_agent_vals(agent)) for agent in agent_ids]
+
                if not agent_ids:
                    record.agent_ids = domain_agent_ids
+               else:
+                   record.agent_ids = agent_ids
