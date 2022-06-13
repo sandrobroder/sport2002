d64447b4141f66641d46a7c96b6c6ee344f5de81
PChectorcerezo <80783603+PChectorcerezo@users.noreply.github.com>
2022-06-13 11:27:24 +0200

    Update product_template.py

--- sport2002_base/models/product_template.py
+++ sport2002_base/models/product_template.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
+import logging
+
 from odoo import fields, models
 import json
+from odoo.exceptions import UserError, ValidationError
+_logger = logging.getLogger(__name__)
 
 
 class ProductTemplate(models.Model):
@@ -41,19 +45,30 @@ class ProductTemplate(models.Model):
             fpos = self.env['account.fiscal.position'].sudo().get_fiscal_position(partner.id)
             taxes = fpos.map_tax(product.sudo().taxes_id.filtered(lambda x: x.company_id == company_id), product,
                                  partner)
-
+            _logger.error("***********************************************")
+            _logger.error("****************** CALCULO TARIFA *****************************")
+            _logger.error("***********************************************")
+            _logger.error("Tax display:")
+            _logger.error(tax_display)
+            _logger.error("Tarifa:")
+            _logger.error(pricelist)
+            _logger.error(pricelist.name)
             # Añado esta linea para que obtenga de nuevo el precio bi del producto ya que sin esta linea, duplica
             # los impuestos por que pasa por la funcion 2 veces.Meter el precio de nuevo nos ayuda a restaurar el
             # funcionamiento correcto
-            list_price = product.price_compute('list_price')[product.id]
-            price = product.price if pricelist else list_price
+#             list_price = product.price_compute('list_price')[product.id]
+#             _logger.error("List Price")
+#             _logger.error(list_price)
+# #             price = product.price if pricelist else list_price
+#             _logger.error("Precio")
+#             _logger.error(price)
 
             # The list_price is always the price of one.
             quantity_1 = 1
-            combination_info['price'] = self.env['account.tax']._fix_tax_included_price_company(
-                price,
-                product.sudo().taxes_id,
-                taxes, company_id)
+#             combination_info['price'] = self.env['account.tax']._fix_tax_included_price_company(
+#                 price,
+#                 product.sudo().taxes_id,
+#                 taxes, company_id)
             price = taxes.compute_all(combination_info['price'], pricelist.currency_id, quantity_1, product, partner)[
                 tax_display]
             if pricelist.discount_policy == 'without_discount':
@@ -72,5 +87,8 @@ class ProductTemplate(models.Model):
                 list_price=list_price,
                 has_discounted_price=has_discounted_price,
             )
+            
+            _logger.error("///////// COMBINACION ///////////")
+            _logger.error(combination_info)
 
         return combination_info
