fc61e77ebce34b572b8dfc957112907580f82c4a
PChectorcerezo <80783603+PChectorcerezo@users.noreply.github.com>
2022-08-04 09:42:16 +0200

    Update sale_cart_link.js

--- pc_sport2002_temporary_fixed_add_to_cart/static/src/js/frontend/sale_cart_link.js
+++ pc_sport2002_temporary_fixed_add_to_cart/static/src/js/frontend/sale_cart_link.js
@@ -16,8 +16,17 @@ odoo.define('pc_sport2002_temporary_fixed_add_to_cart.sale_cart_link', function(
 
     sAnimations.registry.WebsiteSale.include({
         _onClickAdd: function (ev) {
-//            this._super.apply(this, arguments);
-            alert("SI")
-        }
+            ev.preventDefault();
+            var def = () => {
+                this.isBuyNow = $(ev.currentTarget).attr('id') === 'buy_now';
+                return this._handleAdd($(ev.currentTarget).closest('form'));
+            };
+//             if ($('.js_add_cart_variants').children().length) {
+//                 return this._getCombinationInfo(ev).then(() => {
+//                     return !$(ev.target).closest('.js_product').hasClass("css_not_available") ? def() : Promise.resolve();
+//                 });
+//             }
+            return def();
+        },
     });
 });
