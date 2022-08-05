f9a40a3226322c1f523cab5933430e0a93dc371d
gquispepc <gerardo.quispe@processcontrol.es>
2022-08-05 08:58:51 -0500

    [ADD]processcontrol_odoo_studio

---
+++ processcontrol_odoo_studio/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'ProcessControl | Odoo Studio Inherit',
+    'version': '1.0.0',
+    'author': "ProcessControl",
+    'website': "https://www.processcontrol.es",
+    'description': """""",
+    'depends': ['base'],
+    'installable': True,
+    'auto_install': False,
+    'data': [
+        'views/odoo_studio.xml',
+    ]
+}
