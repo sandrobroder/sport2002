c5d742cad50d357bb72d0b3afa25dee34b18be86
gquispepc <gerardo.quispe@processcontrol.es>
2022-10-05 10:04:08 -0500

    [ADD]processcontrol_product_image -> product.template always have a picture

---
+++ processcontrol_product_image/models/product_product.py
@@ -0,0 +1,21 @@
+from odoo.exceptions import ValidationError
+from odoo import api, fields, models, _
+
+
+class ProductProduct(models.Model):
+    _inherit = 'product.product'
+
+    @api.constrains('image_1920')
+    def get_image_field(self):
+        for rec in self:
+            #variants = []
+            image = False
+            for var in rec.product_tmpl_id.product_variant_ids:
+                if var.image_1920:
+                    image = var.image_1920
+                else:
+                    pass
+            if not rec.product_tmpl_id.image_1920:
+                rec.product_tmpl_id.image_1920 = image
+            else:
+                pass
