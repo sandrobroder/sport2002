6c834742187132537c055f5e5919522c2a05d485
neha453 <neha.gupta453@webkul.in>
2023-11-28 20:28:13 +0530

    FIX pre-migration script

--- base_bank_from_iban/migrations/16.0.2.0.0/post-migration.py
+++ base_bank_from_iban/migrations/16.0.2.0.0/post-migration.py
@@ -4,12 +4,12 @@ _logger = logging.getLogger(__name__)
 
 
 @openupgrade.migrate()
-def migrate(cr, version):
+def migrate(env, version):
     """Convert the former `agent_line` m2m relation in `commission.line.mixin` into
     the new `settlement_line_ids` o2m relation."""
     _logger.info(f'Starting Post migration from version {version}.')
     openupgrade.logged_query(
-        cr,
+        env.cr,
         """
             UPDATE commission_settlement_line
             SET invoice_agent_line_id = sal_rel.agent_line_id
