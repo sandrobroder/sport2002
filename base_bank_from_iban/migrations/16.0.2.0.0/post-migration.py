bb83408f41471c48619348f4684808a1c2414a8d
neha453 <neha.gupta453@webkul.in>
2023-11-28 20:36:23 +0530

    FIX post-migration script

--- base_bank_from_iban/migrations/16.0.2.0.0/post-migration.py
+++ base_bank_from_iban/migrations/16.0.2.0.0/post-migration.py
@@ -1,24 +1,24 @@
-from openupgradelib import openupgrade
-import logging
-_logger = logging.getLogger(__name__)
+# from openupgradelib import openupgrade
+# import logging
+# _logger = logging.getLogger(__name__)
 
 
-@openupgrade.migrate()
-def migrate(env, version):
-    """Convert the former `agent_line` m2m relation in `commission.line.mixin` into
-    the new `settlement_line_ids` o2m relation."""
-    _logger.info(f'Starting Post migration from version {version}.')
-    openupgrade.logged_query(
-        env.cr,
-        """
-            UPDATE commission_settlement_line
-            SET invoice_agent_line_id = sal_rel.agent_line_id
-            FROM (
-                SELECT DISTINCT ON (agent_line_id) agent_line_id, settlement_id
-                FROM settlement_agent_line_rel
-                ORDER BY agent_line_id
-            ) AS sal_rel
-            WHERE id = sal_rel.settlement_id
-        """,
-    )
-    _logger.info('Migration completed.')
+# @openupgrade.migrate()
+# def migrate(env, version):
+#     """Convert the former `agent_line` m2m relation in `commission.line.mixin` into
+#     the new `settlement_line_ids` o2m relation."""
+#     _logger.info(f'Starting Post migration from version {version}.')
+#     openupgrade.logged_query(
+#         env.cr,
+#         """
+#             UPDATE commission_settlement_line
+#             SET invoice_agent_line_id = sal_rel.agent_line_id
+#             FROM (
+#                 SELECT DISTINCT ON (agent_line_id) agent_line_id, settlement_id
+#                 FROM settlement_agent_line_rel
+#                 ORDER BY agent_line_id
+#             ) AS sal_rel
+#             WHERE id = sal_rel.settlement_id
+#         """,
+#     )
+#     _logger.info('Migration completed.')
